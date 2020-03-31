a1a93845aabdc1e5b5ab7bc606b69447a8978530
Kaahan <kaahan15@gmail.com>
2020-03-31 14:19:59 -0500

    Add functionality so that the number of images correctly classified can be tracked

--- src/classifier.cc
+++ src/classifier.cc
@@ -5,8 +5,24 @@
 #include <bayes/model.h>
 
 namespace bayes {
+  int count = 0;
   void ReadModelData(ifstream& test_images_stream, ifstream& test_labels_stream, vector< vector< vector<double> > >&
   pixel_probabilities, vector<double> priors) {
+
+    string label_line2;
+    double value;
+    vector<double> test_labels;
+    while (std::getline(test_labels_stream, label_line2)) {
+      value = std::stoi(label_line2);
+      test_labels.push_back(value);
+
+    }
+    cout << test_labels[0] << endl;
+    cout << test_labels[1] << endl;
+    cout << test_labels[2] << endl;
+    cout << test_labels[998] << endl;
+    cout << test_labels[999] << endl;
+
 
     std::ifstream model_data("data/model_probabilities.csv");
     std::istream& input_stream = model_data;
@@ -18,7 +34,9 @@ namespace bayes {
 
     // will be used to store posterior probabilities for each class for a single image
     vector<double> posterior_probabilities(kNumDigits, 0);
-
+//    for (size_t i = 0; i < kNumDigits; i++) {
+//      posterior_probabilities[i] += log10(priors[i]);
+//    }
 
 
     int row = 0;
@@ -52,12 +70,11 @@ namespace bayes {
 
       if (row == 28) {
 
-//        for (size_t i = 0; i < kNumDigits; i++) {
-//          posterior_probabilities[i] += log10(priors[i]);
-//        }
-        classify(posterior_probabilities);
-        //count++;
+
+        classify(posterior_probabilities, test_labels);
         row = 0;
+        // count keeps track of what image we are on in the test images
+        count++;
         std::fill(posterior_probabilities.begin(), posterior_probabilities.end(), 0);
       }
     }
@@ -65,8 +82,9 @@ namespace bayes {
 
   }
 
-  void classify(vector<double>& posterior_probabilities) {
-    vector<double> classified_images;
+
+  void classify(vector<double>& posterior_probabilities, vector<double>& test_labels) {
+    //vector<double> classified_images;
     double classified = 0;
     for (size_t i = 0; i < kNumDigits; i++) {
       if (posterior_probabilities[i] > posterior_probabilities[classified]) {
@@ -74,11 +92,32 @@ namespace bayes {
       }
     }
 
-    classified_images.push_back(classified);
-    cout << classified_images[0] << endl;
+    //classified_images.push_back(classified);
+    cout << classified << endl;
 
 
+    if (classified == test_labels[count]) {
+      cout << "correct" << endl;
+    }
 
+    
+//    string label_line;
+//    int value;
+//    int count_of_while_loop = 0;
+//    cout << count << endl;
+//    while (std::getline(test_labels_stream, label_line)) {
+//      value = std::stoi(label_line);
+//      cout << count_of_while_loop << endl;
+//      if (count_of_while_loop == count) {
+//        cout << "reached" << endl;
+//        if (value == classified) {
+//          cout << "correct!" << endl;
+//        }
+//      }
+//      count_of_while_loop++;
+//    }
+
+    //count++;
   }
 }  // namespace bayes
 
